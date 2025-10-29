4907b4584aed56197a99ca080801024a6cdecb72
usaib21 <peerusaib7@gmail.com>
2025-10-30 02:04:57 +0530

    updated

--- budget-tracker-backend/backend/finance/apps.py
+++ budget-tracker-backend/backend/finance/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class FinanceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    ame = 'backend.finance'
+    name = 'backend.finance'
+
